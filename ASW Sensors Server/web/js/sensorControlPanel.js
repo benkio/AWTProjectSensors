dbd3f39c6c97ad22f59c1a4de4e3caa3ba7ae33f
Enrico Benini <e.benini@apexnet.it>
2015-03-20 10:19:55 +0100

    Add a function to render the sensor.

--- ASW Sensors Server/web/js/sensorControlPanel.js
+++ ASW Sensors Server/web/js/sensorControlPanel.js
@@ -5,3 +5,8 @@
  */
 
 
+function sensorToHTML(sensorID, sensorStatus, playable, contextPath) {
+    var icon = contextPath + "/img/stopIcon.png";
+    if (playable) icon = contextPath + "/img/playIcon.png";
+    return "<tr><td><p name=\"SensorName"+ sensorID + "\" >"+sensorID+"</p></td><td><p name=\"SensorStatus"+sensorID+"\" >"+sensorStatus+"</p></td><td><input name=\"SensorControlButton"+sensorID+"\" type=\"image\" src="+icon+" height=\"32\" width=\"32\"></td></tr>";   
+}
