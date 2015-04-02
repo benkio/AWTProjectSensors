245fc3e239b38cd354035f202858c41348a855aa
Enrico Benini <e.benini@apexnet.it>
2015-04-02 15:31:54 +0200

    See previous commit.

---
+++ ASWLibrary/src/it/unibo/aswProject/libraries/bean/Sensor.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unibo.aswProject.libraries.bean;
+
+/**
+ *
+ * @author Enrico Benini
+ */
+public class Sensor {
+    public String Name;
+    public String Status;
+    public int Value;
+}
---
+++ ASWLibrary/src/it/unibo/aswProject/libraries/bean/SensorList.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unibo.aswProject.libraries.bean;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Enrico Benini
+ */
+public class SensorList {
+    public ArrayList<Sensor> sensorList;
+    
+    public SensorList(){
+        sensorList = new ArrayList<>();
+    }
+    
+}
