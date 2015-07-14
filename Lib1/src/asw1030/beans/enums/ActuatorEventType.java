b80b254383d2b344bb4d1728f7a93177400b13c5
Thomas Farneti <thomas.farneti@gmail.com>
2015-07-14 23:32:51 +0200

    restore lib1 changes

---
+++ Lib1/src/asw1030/beans/enums/ActuatorEventType.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asw1030.beans.enums;
+
+/**
+ *
+ * @author Thomas
+ */
+public enum ActuatorEventType {
+    NEWVALUE
+}
---
+++ Lib1/src/asw1030/beans/interfaces/IActuatorEventsListener.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asw1030.beans.interfaces;
+
+import asw1030.beans.enums.ActuatorEventType;
+
+
+/**
+ *
+ * @author Thomas
+ */
+public interface IActuatorEventsListener {
+    void newEvent(ActuatorEventType ae,Object arg);
+}
